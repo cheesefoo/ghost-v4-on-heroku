b9da44140335597f19b4fb6cc736749fff911e34
cheesefoo <cheesefoo666@gmail.com>
2022-09-03 18:12:49 -0400

    try deploy to render 9

--- bin/init-deployment.sh
+++ bin/init-deployment.sh
@@ -8,5 +8,4 @@ cat "config.production.json"
 
 node ./bin/wait-for-db.js
 
-knex-migrator init --mgpath node_modules/ghost
-knex-migrator migrate --v 4.47 --force
+knex-migrator init --mgpath node_modules/ghost --v 4.47 --force
