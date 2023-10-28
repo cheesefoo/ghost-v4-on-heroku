0200fd1003baff6eaa7210d6206d052c28117ce3
cheesefoo <cheesefoo666@gmail.com>
2023-10-28 19:19:16 -0400

    update amazon cert & server

--- bin/init-deployment.sh
+++ bin/init-deployment.sh
@@ -3,6 +3,7 @@
 echo "Initializing the deployment…"
 echo "pwd →"
 pwd
+wget "https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem" -P /opt/render/project/src/
 echo "config.production.json →"
 cat "config.production.json"
 node ./bin/wait-for-db.js
